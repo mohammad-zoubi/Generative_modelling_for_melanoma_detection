1b109d1b2920f87662be1155b879a2e14222cf6d
rosenanna <anna_frosen@hotmail.com>
2022-04-09 21:29:53 +0200

    Latest update of crop-resize function

--- data_analysis/squaring_cropping.py
+++ data_analysis/squaring_cropping.py
@@ -8,26 +8,41 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg 
 from PIL import Image
 import os, os.path
+import csv
+from tqdm import tqdm 
 
 def circle_center(mask): # returns the center of a white circle in a black frame
   centerx = np.argmax(np.sum(mask, axis=1))
   centery = np.argmax(np.sum(mask, axis=0))
   return centerx, centery
 
-folder = '/ISIC256/ISIC_pool/malignant_all/ORIGINAL_IMAGES'
+# folder = '/ISIC256/ISIC_pool/malignant_all/ORIGINAL_IMAGES'
+folder = '/ISIC256/ISIC_pool/malignant_all/resized'
+
 new_folder = '/ISIC256/ISIC_pool/malignant_all/Cropped_resized'
+# new_folder = '/ISIC256/ISIC_pool/malignant_all/resized'
+
+classifier = '/ISIC256/ISIC_pool/multiclassifier.csv'
 
 size = 256
 
+######## CSV FILE ###############################################
 file_list = os.listdir(folder)
+data = open(classifier)
+feature = csv.reader(data)
+
+feature_list = []
+for row in feature:
+        feature_list.append(row)
+
 # print(file_list)
 
-### CONVERT ALL RECTANGULAR IMAGES ###############################
+
 
 # want to add cropping if multiclassifier determines its a frame
 
 images = []
-for idx in range(0,10): #len(file_list)):
+for idx in tqdm(range(0,10)): #len(file_list))):
     img = cv2.imread(os.path.join(folder,file_list[idx]))
    
     if img is  None:
@@ -36,44 +51,55 @@ for idx in range(0,10): #len(file_list)):
     path_out = os.path.join(new_folder, file_list[idx])
     dimensions = img.shape
 
-    if dimensions[0]!=dimensions[1]:
-        # print('Need cropping')
+### CONVERT ALL RECTANGULAR IMAGES ###############################
 
-        if dimensions[0]<dimensions[1]:
-            crop = dimensions[0]/2
-            img = img[0:dimensions[0],round(dimensions[1]/2-crop):round(dimensions[1]/2+crop)]
-        else:
-            crop = dimensions[1]/2
-            img = img[round(dimensions[0]/2-crop):round(dimensions[0]/2+crop),0:dimensions[1]]
+    # if dimensions[0]!=dimensions[1]:
+    #     # print('Need cropping')
+
+    #     if dimensions[0]<dimensions[1]:
+    #         crop = dimensions[0]/2
+    #         img = img[0:dimensions[0],round(dimensions[1]/2-crop):round(dimensions[1]/2+crop)]
+    #     else:
+    #         crop = dimensions[1]/2
+    #         img = img[round(dimensions[0]/2-crop):round(dimensions[0]/2+crop),0:dimensions[1]]
+
+###### cropping frames ###########################################
+    #if multiclassifier determined there was a frame -> remove it
+
+    if int(feature_list[idx][4]) == 1:
+        # print('current image ,',file_list[idx], ', row, ', feature_list[idx])
+
+        gray = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
+        gray[gray >= 100] = 255
+        kernel = np.ones((5,5),np.float32)
+        dst = cv2.filter2D(gray,-1,kernel)
+        center = circle_center(gray)
+        pi = gray[np.average(gray, axis=0).argmax(), :].tolist().index(255)
+        piHalve = gray[:, np.average(gray, axis=1).argmax()].tolist().index(np.max(gray[:, np.average(gray, axis=1).argmax()]))
+        zeroPi = len(gray[np.average(gray, axis=0).argmax(),:]) - 1 - gray[np.average(gray, axis=0).argmax(),:][::-1].tolist().index(255)
+        onePiHalve = len(gray[np.average(gray, axis=1).argmax(),:]) - 1 - gray[np.average(gray, axis=1).argmax(),:][::-1].tolist().index(255)
+        
+        diameter = max((zeroPi- pi), (onePiHalve - piHalve))
+        radius = round(diameter/2)
+        x1 = int(pi) #int(pi+(radius-radius*np.sqrt(2)/2))
+        y1 = int(piHalve) #int(piHalve+(radius-radius*np.sqrt(2)/2))
+        x2 = int(zeroPi) #int(zeroPi - (radius-radius*np.sqrt(2)/2))
+        y2 = int(onePiHalve) #int(onePiHalve -(radius-radius*np.sqrt(2)/2))
+
+        img = A.Crop(x_min=x1,
+                            y_min=y1, 
+                            x_max=x2,
+                            y_max=y2).apply(img)
 
 
-
-    # gray = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
-    # gray[gray >= 100] = 255
-    # kernel = np.ones((5,5),np.float32)
-    # dst = cv2.filter2D(gray,-1,kernel)
-    # center = circle_center(gray)
-    # pi = gray[np.average(gray, axis=0).argmax(), :].tolist().index(255)
-    # piHalve = gray[:, np.average(gray, axis=1).argmax()].tolist().index(np.max(gray[:, np.average(gray, axis=1).argmax()]))
-    # zeroPi = len(gray[np.average(gray, axis=0).argmax(),:]) - 1 - gray[np.average(gray, axis=0).argmax(),:][::-1].tolist().index(255)
-    # onePiHalve = len(gray[np.average(gray, axis=1).argmax(),:]) - 1 - gray[np.average(gray, axis=1).argmax(),:][::-1].tolist().index(255)
-    
-    # diameter = max((zeroPi- pi), (onePiHalve - piHalve))
-    # radius = round(diameter/2)
-    # x1 = int(pi+(radius-radius*np.sqrt(2)/2))
-    # y1 = int(piHalve+(radius-radius*np.sqrt(2)/2))
-    # x2 = int(zeroPi - (radius-radius*np.sqrt(2)/2))
-    # y2 = int(onePiHalve -(radius-radius*np.sqrt(2)/2))
-
-    # new_image = A.Crop(x_min=x1,
-    #                     y_min=y1, 
-    #                     x_max=x2,
-    #                     y_max=y2).apply(img)
     # img_encoded = cv2.imencode(".png", new_image)[1].tobytes() 
     # nparr = np.frombuffer(img_encoded, np.byte)
 
     # print('Original Dimensions : ',img.shape)
 #     size = int(size)
+
+##### RESIZE ##############################################
+
     width = size
     height = size
     # print(isinstance(size, str))
@@ -84,145 +110,3 @@ for idx in range(0,10): #len(file_list)):
     
     # WRITE TO FOLDER
     cv2.imwrite(path_out, img)
-
-
-        
-    
-
-
-
-# image_path = '/SAM256/SAM_ORIGINAL_TV/Full_size/'
-# path_out = '/SAM256/SAM_ORIGINAL_TV/256_full_cropped'
-# size = 256
-# ###################################
-# # ADD ext to json file 
-
-# def circle_center(mask): # returns the center of a white circle in a black frame
-#   centerx = np.argmax(np.sum(mask, axis=1))
-#   centery = np.argmax(np.sum(mask, axis=0))
-#   return centerx, centery
-
-# with open('dataset.json', 'r') as file:
-#     # print("json.load(file): ", json.load(file))
-#     labels = json.load(file)["labels"]
-#     for img in labels:
-#         img[0] = img[0]+'.png'
-# labels_list_dict = { "labels" : labels}
-
-# with open("/SAM256/SAM_ORIGINAL_TV/dataset.json", "w") as outfile:
-#     json.dump(labels_list_dict, outfile)
-
-# file_list = os.listdir(image_path)
-# print(file_list)
-###################################
-
-# list_of_files = zip_ref.namelist()
-# Loop through the list of files 
-# for idx in range(1,len(file_list)): # list of files has all the files in zip including the folders
-#     # Get the extension of the the given file and if it is an image, excute what is inside the if statement 
-
-#     path_in = os.path.join(image_path, file_list[idx]) 
-#     # if os.path.isfile(path_in) == False:
-#     #     continue
-#     img = cv2.imread(path_in)
-
-
-
-#     # in_bytes = zip_ref.read(list_of_files[idx])
-#     # img = cv2.imdecode(np.frombuffer(in_bytes, np.uint8), cv2.IMREAD_COLOR)
-#     # img = hair_removal(img, kern=10, intensity=5)
-
-#     # img = hair_removal(img, kern=10, intensity=5) # is it really needed ???
-#     # get mask of an image -> get center of a circle -> get square indices 
-#     # cut the square out 
-#     # profit ??
-#     # mask = mask_extractor(img, kern, can, pad)
-#     # cx, cy = circle_center(mask)
-#     # x1, y1, x2, y2 = circle_to_square_crop(mask, cx, cy, shift)
-
-#     gray = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
-#     gray[gray >= 100] = 255
-#     kernel = np.ones((5,5),np.float32)
-#     dst = cv2.filter2D(gray,-1,kernel)
-#     center = circle_center(gray)
-#     pi = gray[np.average(gray, axis=0).argmax(), :].tolist().index(255)
-#     piHalve = gray[:, np.average(gray, axis=1).argmax()].tolist().index(np.max(gray[:, np.average(gray, axis=1).argmax()]))
-#     zeroPi = len(gray[np.average(gray, axis=0).argmax(),:]) - 1 - gray[np.average(gray, axis=0).argmax(),:][::-1].tolist().index(255)
-#     onePiHalve = len(gray[np.average(gray, axis=1).argmax(),:]) - 1 - gray[np.average(gray, axis=1).argmax(),:][::-1].tolist().index(255)
-    
-#     diameter = max((zeroPi- pi), (onePiHalve - piHalve))
-#     radius = round(diameter/2)
-#     x1 = int(pi+(radius-radius*np.sqrt(2)/2))
-#     y1 = int(piHalve+(radius-radius*np.sqrt(2)/2))
-#     x2 = int(zeroPi - (radius-radius*np.sqrt(2)/2))
-#     y2 = int(onePiHalve -(radius-radius*np.sqrt(2)/2))
-
-#     new_image = A.Crop(x_min=x1,
-#                         y_min=y1, 
-#                         x_max=x2,
-#                         y_max=y2).apply(img)
-#     # img_encoded = cv2.imencode(".png", new_image)[1].tobytes() 
-#     # nparr = np.frombuffer(img_encoded, np.byte)
-
-#      # print('Original Dimensions : ',img.shape)
-#     size = int(size)
-#     width = size
-#     height = size
-#     # print(isinstance(size, str))
-#     dim = (width, height)
-    
-#     # resize image
-#     resized = cv2.resize(new_image, dim, interpolation = cv2.INTER_LANCZOS4)
-    
-#     # print('Resized Dimensions : ',resized.shape)
-    
-#     # plt.imshow(resized)
-#     # plt.show()
-    
-#     # cv2_imshow(img)
-#     if size ==256:
-#         path_out = os.path.join('/SAM256/SAM_ORIGINAL_TV/256_full_cropped', file_list[idx])
-
-#     # WRITE TO FOLDER
-#     cv2.imwrite(path_out, resized)
-
-
-    # image_name = os.path.splitext(list_of_files[idx])[0].split('/')[2]
-    # processed_img_in_situ_zip.writestr(image_name+".png", nparr)
-
-
-########################################################################
-
-# image_path = "/ISIC256/ISIC_pool/malignant_all/ORIGINAL_IMAGES"
-# file_list = os.listdir(image_path)
-
-# for m in range(len(file_list)):
-    
-#     path_in = os.path.join(image_path, file_list[m]) 
-#     # if os.path.isfile(path_in) == False:
-#     #     continue
-#     img = cv2.imread(path_in)
-    
-#     # print('Original Dimensions : ',img.shape)
-#     # size = int(size)
-#     # width = size
-#     # height = size
-#     # # print(isinstance(size, str))
-#     # dim = (width, height)
-    
-#     # resize image
-#     resized = cv2.resize(img, (256, 256), interpolation = cv2.INTER_LANCZOS4)
-    
-#     # print('Resized Dimensions : ',resized.shape)
-    
-#     # cv2_imshow(resized)
-#     # cv2_imshow(img)
-#     # if size ==256:
-#     path_out = os.path.join("/ISIC256/ISIC_pool/malignant_all/RESIZED_IMAGES/", file_list[m])
-#     # elif size == 512:
-#     # path_out = os.path.join('//content/drive/MyDrive/thesis/ELLA_Eivor1.0_DERM_dataset.zip (Unzipped Files)/Sahlgrenska/processed_512', "{:04d}".format(m)+ '_derm_01.jpeg')
-#     # else:
-#     # print("wrong dimensions")
-#     # sys.exit()
-
-#     cv2.imwrite(path_out, resized)
